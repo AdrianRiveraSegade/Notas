545ed566e0d93c955d4c4665a979f1b02b8246d6
segaderiveraadrian <segaderiveraadrian@gmail.com>
2023-01-03 18:23:51 +0100

    cambio menor

--- QUERIES/users/selectUserByEmailQuery.js
+++ QUERIES/users/selectUserByEmailQuery.js
@@ -1,18 +1,18 @@
 const getConnection = require("../../BBDD/getConnection");
 const { generateError } = require("../../helpers");
-const selectUserByIdQuery = async (idUser) => {
+const selectUserByIdQuery = async (email) => {
   let connection;
 
   try {
     connection = await getConnection();
 
     const [users] = await connection.query(
-      `SELECT id, email, createdAt FROM users WHERE id = ?`,
-      [idUser]
+      `SELECT id, email, createdAt FROM users WHERE email = ?`,
+      [email]
     );
 
     if (users.length < 1) {
-      throw generateError("Usuario no encontrado", 404);
+      throw generateError("Usuario no encontrado/email incorrecto", 404);
     }
 
     return users[0];
