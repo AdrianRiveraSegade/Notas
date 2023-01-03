9ae3c7476a41ddd10a94849c653ee42689a865b6
segaderiveraadrian <segaderiveraadrian@gmail.com>
2023-01-03 16:28:34 +0100

    nuevo query, funciona?

---
+++ QUERIES/notes/deleteEntryNotesQuery.js
@@ -0,0 +1,25 @@
+const getConnection = require("../../BBDD/getConnection");
+const { generateError } = require("../../helpers");
+
+const deleteEntryNotesQuery = async (users_id, title) => {
+  let connection;
+
+  try {
+    connection = await getConnection();
+
+    //seleccionamos la nota
+    const [deleteEntryNotes] = await connection.query(
+      `SELECT users_id FROM entryNotes WHERE users_id = ?`,
+      [users_id]
+    );
+    if (deleteEntryNotes.users_id !== users_id) {
+      throw generateError("No tienes permisos", 401);
+    }
+    //borramos la nota
+    await connection.query(`DELETE FROM entryNotes WHERE title = ?`, [title]);
+  } finally {
+    if (connection) connection.release();
+  }
+};
+
+module.exports = deleteEntryNotesQuery;
--- USERS/newUser.js
+++ USERS/newUser.js
@@ -1,23 +1,23 @@
-const { v4: uuid } = require('uuid');
+const { v4: uuid } = require("uuid");
 
-const { generateError } = require(../helpers.js);
+const { generateError } = require("../helpers.js");
 
 const newUser = async (req, res, next) => {
   try {
     const { username, email, password } = req.body;
 
-    if ( !username || !email || !password) {
-        throw generateError('Faltan campos', 400);
+    if (!username || !email || !password) {
+      throw generateError("Faltan campos", 400);
     }
 
     //Generamos un código de registro de usuario
     const registrationCode = uuid();
 
     res.send({
-        status:'ok',
-        data: {
-            registrationCode,
-        },
+      status: "ok",
+      data: {
+        registrationCode,
+      },
     });
   } catch (err) {
     next(err);
